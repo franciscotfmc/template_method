dd5d72929352553ea7cfac8b3ff24a30c6fbd170
Francisco Thiene <f.thiene@gmail.com>
2013-06-25 19:04:05 -0300

    Use argparse to receive args from input

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 #/usr/bin/env python2.7
 #coding: utf-8
 
-from patterns.template import HtmlDocument
+import argparse
+from patterns.template import HtmlDocument, CsvDocument
 from transaction import Product
 
 def main():
-  print HtmlDocument([Product("Gasolina", 3.20), Product("Gás Natural", 2.10), Product("Diesel", 2.20)]).generate_doc()
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--format', help='the output format can be html or csv')
+  args = parser.parse_args()
+
+  items = [Product("Gasolina", 3.20), Product("Gás Natural", 2.10), Product("Diesel", 2.20)]
+  
+  if args.format == "html":
+    document = HtmlDocument(items)
+  else:
+    document = CsvDocument(items)
+
+  output = document.generate_doc()
+  f_output = open("output.{0}".format(args.format),"w")
+  f_output.write(output)
+  f_output.close()
 
 if __name__ == '__main__':
   main()
