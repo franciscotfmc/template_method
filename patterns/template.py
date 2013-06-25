000bcf9e3efed38cac79623852ca88c1c4a8707a
Francisco Thiene <f.thiene@gmail.com>
2013-06-24 23:41:33 -0300

    Implement template pattern

---
+++ patterns/template.py
@@ -0,0 +1,41 @@
+#coding: utf-8
+
+from abc import ABCMeta, abstractmethod
+
+class DocumentMaker:
+  __metaclass__ = ABCMeta
+
+  def __init__(self, transactions):
+    self.transactions = transactions
+
+  def generate_doc(self):
+    return "%s %s %s" % (self.make_header(), self.make_body(), self.make_footer())
+
+  @abstractmethod
+  def make_header(self): 
+    pass
+
+  @abstractmethod
+  def make_body(self): 
+    pass
+
+  @abstractmethod
+  def make_footer(self):
+    pass
+
+class HtmlDocument(DocumentMaker):
+
+  def make_header(self):
+    return "<html><head><title>Template Method</title></head>"
+
+  def make_body(self):
+    content = "<h1>Relatório de Transações</h1><br><ul>"
+    for t in self.transactions:
+      content += "<li>%s - %s</li>" % (t.description, t.value)
+    
+    content += "</ul>"
+    return "<body><strong>%s</strong>" % content
+
+  def make_footer(self):
+    return "<hr>Total: %s </body></html>" % reduce(lambda x,y : x + y, [t.value for t in self.transactions])
+
---
+++ transaction.py
@@ -0,0 +1,7 @@
+#coding: utf-8
+
+class Product(object):
+  
+  def __init__(self, description, value):
+    self.description = description
+    self.value = value
